e3c675d37376db594ca0c5a8da51212513733057
Takashi Makimoto <mackie@beehive-dev.com>
2023-05-27 03:35:03 +0900

    add minimal code for TailwindCSS

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+const postcssConfig = {
+  plugins: {
+    autoprefixer: {},
+    tailwindcss: {},
+  },
+};
+
+module.exports = postcssConfig;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type { import('tailwindcss').Config } */
+const tailwindcssConfig = {
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './src/**/*.{js,ts,jsx,tsx,mdx}'],
+  plugins: [],
+  theme: {
+    extend: {},
+  },
+};
+
+module.exports = tailwindcssConfig;
